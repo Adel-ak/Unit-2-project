cb3a49f1c2b19ad18a6344af20fe182b668d957d
Adel ak <adoolak@gmail.com>
2019-03-29 22:05:46 +0300

    finalized (no bugs found)

--- js/script.js
+++ js/script.js
@@ -3,137 +3,140 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-let notFound = [];// this array will hold whats not displied
-
-//global variables 
-const listUl = document.querySelector('.student-list');
-const studentLi = document.querySelectorAll('.student-item');
-for(let i = 0; i < studentLi.length; i++){
-   studentLi[i].classList.add('show');
-}
+//Global variables (used by multi functions)
 const studentDiv = document.querySelector('.page');
-//
-
-//Search button on the main page
 const headerDiv = document.querySelector('.page-header');
-const buttonSpan = document.createElement('span');
-buttonSpan.className = 'student-search';
-buttonSpan.innerHTML = `<input  type="text" placeholder="Search for students..." onkeyup="search();">
-                  <button type="submit" name="submit" value="submit">Search</button>`;
-headerDiv.appendChild(buttonSpan);
-//
-
-//message for when no result is found, added to the students <ul> in the page and its display is set to "none"
-const noMatchLi = document.createElement('li');
-noMatchLi.className = 'no-match';
-const noMatchText = document.createElement('h1');
-noMatchText.textContent = 'Sorry No Match Found......';
-noMatchLi.appendChild(noMatchText);
-noMatchLi.style.display = 'none';
-listUl.appendChild(noMatchLi);
+const ulPagination = document.createElement('ul');
+const studentsLi = document.querySelectorAll('.student-item');
+let pageNumber = 1;
+let displayed = studentsLi;
+let notDisplayed = []; //this array will hold the list which is not displayed
+let noMatchLi; //this variable holds the message that will diplay when no match is found
+let amountPerPage; //this variable holds a number which will be used to create the max amount of <a> links 
 //
 
 
-//function repsonable for
-const search = () => {
-   const removePagination = studentDiv.lastElementChild;
-   const searchInput = document.querySelector('.student-search input');
-   const studentNames = document.querySelectorAll('.student-item h3');
-   notFound = [];//sets array empty
-   for(let i = 0; i < studentLi.length; i++){
-      //checks if input value if found in studentNames
-      if(studentNames[i].textContent.includes(searchInput.value)){
-         const print = studentLi[i];
-         print.style.display = '';
-         studentLi[i].classList.add('show');
-      }
-      //checks if input value is not found in studentNames
-      if(!studentNames[i].textContent.includes(searchInput.value)){
-         const notPrint = studentLi[i];
-         notPrint.style.display = 'none';
-         studentLi[i].classList.remove('show');
-         notFound.push(notPrint);//pushing whats not displied into notFound array 
-      }
-   }
-   /***
-   checks if the array check length is equal to studentNames length,
-   if its equal then it will diplay noMatchLi because no students is found 
-   ***/
-   if(notFound.length === studentNames.length){
-      noMatchLi.style.display = '';
-   }else{
-      noMatchLi.style.display = 'none';
-   }
-   studentDiv.removeChild(removePagination);//removes Pagination div
-   appendPageLinks();//adds new Pagination div once more with the correct amount of links
-   
-}
-//
+//object which holds all the functions
+const run = {
+    //This function itterates through the student list and display only 10 per page(eg 1 to 10,30 to 40), even if the list is under 10
+    showPage: (list, pageNum) => {
+        let startIndex = (10 * pageNum) - 10; //first index of list
+        let endIndex = (10 * pageNum) - 1; //last index of list
+        for (let i = 0; i < list.length; i++) {
+            if (i >= startIndex && i <= endIndex) {
+                list[i].style.display = ''; //list that will be shown
+            } else {
+                list[i].style.display = 'none'; //list that will be not shown
+            }
+        }
+    },
+    //This function adds Pagination to the main div
+    appendPageLinks: (list) => {
+        amountPerPage = Math.ceil(list.length / 10); //set the amount of PerPage
+        const divPagination = document.createElement('div');
+        divPagination.className = 'pagination';
+        for (let i = 1; i <= amountPerPage; i++) {
+            ulPagination.innerHTML += `<li><a>${i}</a></li>`; //amount of pagination links to be added to the page
+        }
+        divPagination.appendChild(ulPagination);
+        studentDiv.appendChild(divPagination);
+        //event listener added to div with the class "page-header"
+        ulPagination.addEventListener("click", (e) => {
+            const currentActive = document.getElementsByClassName("active"); //holds the <a> links which were clicked
+            //checks if the <a> tags is clicked
+            if (e.target.tagName === 'A') {
+                //removes active class from previous <a> links
+                for (var i = 0; i < currentActive.length; i++) {
+                    currentActive[i].className = "";
+                }
+                //adds the active class to the clicked <a> link and passes its number to the pageNumber to show the selected page
+                e.target.className = "active";
+                pageNumber = e.target.textContent; //page number is set to the number of the clicked link to go to the next/previous page
+                run['showPage'](displayed, pageNumber); //showPage runs after the new page number is selected
+            }
+        });
+        //
+    },
+    //this function is repsonable for searching the students
+    search: () => {
+        pageNumber = 1; //pageNumber set back to '1' so the frist page is always selected after each search
+        const searchInput = document.querySelector('input');
+        const studentsNames = document.querySelectorAll('h3');
+        const a = document.getElementsByTagName('a');
+        notDisplayed = []; //sets array empty
+        displayed = []; //sets displayed into an empty array
 
-//event listener added to div with the class "page-header"
+        for (let i = 0; i < studentsLi.length; i++) {
+            //checks if input value is found in studentsNames if so set to studentsLi diplay to diplay
+            if (studentsNames[i].textContent.includes(searchInput.value)) {
+                studentsLi[i].style.display = '';
+                displayed.push(studentsLi[i]); //pushing whats displayed into displayed array to find out whats the new amount of the list 
+                amountPerPage = Math.ceil(displayed.length / 10); //set the new amount PerPage  
+                //checks if input value is not found in studentsNames if so set to studentsLi diplay to none
+            } else if (!studentsNames[i].textContent.includes(searchInput.value)) {
+                studentsLi[i].style.display = 'none';
+                notDisplayed.push(studentsLi[i]); //pushing whats not displayed into notFound array (to be used to check if noMatchLi is needed to be display by checking 'notDisplayed.length === studentsNames.length')
+            }
+        }
+
+        //checks if the array notDisplayed length equal to studentsNames length set diplay noMatchLi to diplay because no students is found 
+        if (notDisplayed.length === studentsNames.length) {
+            noMatchLi.style.display = '';
+        } else if (notDisplayed.length !== studentsNames.length) {
+            noMatchLi.style.display = 'none';
+        }
+
+        //checks if diplied list less then or equal 10 if true set the diplay for Pagination to none, no Pagination needed max amount of list is 10 
+        if (displayed.length <= 10) {
+            ulPagination.style.display = 'none';
+        } else {
+            ulPagination.style.display = '';
+        }
+
+        //checks if <a> links are more then amountPerPage,if ture set display of empty link to 'none'
+        for (let j = 0; j < a.length; j++) {
+            const off = a[j].parentNode;
+            if (j >= amountPerPage) {
+                off.style.display = 'none';
+                a[j].className = "";
+            } else if (j <= amountPerPage) {
+                off.style.display = '';
+                a[j].className = "";
+            }
+        }
+
+        run['showPage'](displayed, pageNumber); //after each search it will display to page 1 with the new list from the resualt is searching 
+    },
+    //function that holds functions,Search button and noMatchLi
+    startUp: () => {
+        //Search button on the main page
+        const buttonSpan = document.createElement('span');
+        buttonSpan.className = 'student-search';
+        //input is set to onClick 
+        buttonSpan.innerHTML = `<input  type="text" placeholder="Search for students..." onkeyup="run['search']();">
+                              <button type="submit" name="submit" value="submit">Search</button>`;
+        headerDiv.appendChild(buttonSpan);
+        //message for when no result is found, added to the students <ul> in the page and its display is set to "none"
+        const listUl = document.querySelector('.student-list');
+        noMatchLi = document.createElement('li');
+        noMatchLi.className = 'no-match';
+        const noMatchText = document.createElement('h1');
+        noMatchText.textContent = 'Sorry No Match Found......';
+        noMatchLi.appendChild(noMatchText);
+        noMatchLi.style.display = 'none';
+        listUl.appendChild(noMatchLi);
+        
+        run['appendPageLinks'](displayed);
+        run['showPage'](displayed, pageNumber);
+    }
+};
+
+//event listener added to div with the class "page-header" which will listen for clicks from a button.
 headerDiv.addEventListener('click', (e) => {
-   const button = e.target;
-   //checks if the button is clicked
-   if(button.tagName === 'BUTTON'){
-      search();
-   } 
+    //checks if the button is clicked
+    if (e.target.tagName === 'BUTTON') {
+        run['search']();
+    }
 });
-//
 
-// 
-const appendPageLinks = (list) => {
-   //Pagination added to the main div checks for <li>'s with the class 'show' to determine how much <a> links is needed
-   const liShow = document.querySelectorAll('.show')
-   const amountPerPage = Math.ceil(liShow.length / 10) - 1;
-   const divPagination = document.createElement('div');
-   const ulPagination = document.createElement('ul');
-   divPagination.className = 'pagination';
-   for(let i = 1; i <= amountPerPage +1; i++){  
-     ulPagination.innerHTML += `<li><a>${i}</a></li>`; 
-   }
-   divPagination.appendChild(ulPagination);
-   studentDiv.appendChild(divPagination);
-   //
-
-   //event listener added to ulPagination links and checks clicks for <a> link
-   ulPagination.addEventListener("click", (e) => {
-      const currentActive = document.getElementsByClassName("active");
-      if(e.target.tagName === 'A'){
-         //removes active class from previs <a> links
-         for(var i = 0; i < currentActive.length; i++){
-            currentActive[i].className = " ";
-         }
-         e.target.className = "active";
-      } 
-   });
-   return divPagination
-}
-//
-
-//
-const showPage = (list) => {
-/*** 
-Create the `showPage` function to hide all of the items in the 
-list except for the ten you want to show.
-
-Pro Tips: 
-   - Keep in mind that with a list of 54 students, the last page 
-      will only display four.
-   - Remember that the first student has an index of 0.
-   - Remember that a function `parameter` goes in the parens when 
-   you initially define the function, and it acts as a variable 
-   or a placeholder to represent the actual function `argument` 
-   that will be passed into the parens later when you call or 
-   "invoke" the function 
-Loop over items in the list parameter
--- If the index of a list item is >= the index of the first
-item that should be shown on the page
--- && the list item index is <= the index of the last item
-that should be shown on the page, show it
-***/
-}
-//
-
-window.onload = appendPageLinks();//adds the frist Pagination links when page is opened
+window.onload = run['startUp'](); //starts functions and adds serch buttuon/noMatchLi when window is opened
